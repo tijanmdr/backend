20a90ecc4a0050ffdcd9c9ec7ccce1ccbd717dcc
tijan <tijanmdR@gmail.com>
2024-04-22 22:58:20 +1000

    schema updates

--- db/migrations/20240416112108_create_users_table.sql
+++ db/migrations/20240416112108_create_users_table.sql
@@ -1,11 +1,11 @@
 -- migrate:up
 create table users (
-  id integer auto_increment PRIMARY KEY,
+  id integer auto_increment PRIMARY KEY not null,
   name varchar(255),
-  email varchar(255) not null, 
+  email varchar(255) not null unique, 
   password varchar(255) not null, 
-  created_at datetime, 
-  updated_at datetime
+  created_at timestamp default current_timestamp, 
+  updated_at datetime default current_timestamp on update current_timestamp
 );
 
 -- migrate:down
