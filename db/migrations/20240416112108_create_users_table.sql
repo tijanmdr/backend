70afd52960d5cd7199d665c6e5b5975e6df1a952
tijan <tijanmdR@gmail.com>
2024-04-23 22:04:16 +1000

    added type to the user for differentiate the students and instructors

--- db/migrations/20240416112108_create_users_table.sql
+++ db/migrations/20240416112108_create_users_table.sql
@@ -3,7 +3,8 @@ create table users (
   id integer auto_increment PRIMARY KEY not null,
   name varchar(255),
   email varchar(255) not null unique, 
-  password varchar(255) not null, 
+  password varchar(255) not null,
+  type integer not null default 0, /* 0 for instructor and 1 for students */
   created_at timestamp default current_timestamp, 
   updated_at datetime default current_timestamp on update current_timestamp
 );
