4256e425878d642682dffc97f6229499800ce9cb
tijan <tijanmdR@gmail.com>
2024-04-25 00:33:28 +1000

    seeder fixes along with main user registration

--- src/config/seeder.js
+++ src/config/seeder.js
@@ -8,16 +8,26 @@ async function seed() {
     const hash = await argon2.hash('password');
 
     try {
-        const [result] = await connection.execute(
+        await connection.beginTransaction();
+        const [user] = await connection.execute(
             "INSERT INTO users (name, email, password, type) VALUES (?, ?, ?, ?)",
             ['Tijan', 'tijanmdr@gmail.com', hash, db.user_types['instructor']]
         );
 
-        if (result.affectedRows === 1) {
-            console.log('Database seed successful!');
-            process.exit(0);
+        if (user.affectedRows === 1) {
+            const [user_details] = await connection.execute(
+                "insert into user_details (user, phone, address, dob, license, license_expiry) values (?,?,?,?,?,?)",
+                [user.insertId, '021031023', '1283909123', new Date('1994-1-2'), '23232323', new Date('2025-1-2')]
+            );
+
+            if (user_details.affectedRows === 1) {
+                console.log('Database seed successful!');
+                await connection.commit();
+                process.exit(0);
+            }
         }
     } catch (err) {
+        await connection.rollback();
         console.error(`error: ${err.message}`);
         process.exit(0);
     }
